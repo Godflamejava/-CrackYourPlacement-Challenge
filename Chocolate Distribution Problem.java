abde54f031b46bd50dce753ea3007fbfd7176cd8
Godflamejava <58296857+Godflamejava@users.noreply.github.com>
2022-05-04 18:00:42 +0530

    Update Chocolate Distribution Problem.java

--- Chocolate Distribution Problem.java
+++ Chocolate Distribution Problem.java
@@ -1,39 +1,27 @@
-// JAVA Code For Chocolate Distribution
-// Problem
+
 import java.util.*;
 
-class GFG {
+class solve {
 	
-	// arr[0..n-1] represents sizes of
-	// packets. m is number of students.
-	// Returns minimum difference between
-	// maximum and minimum values of
-	// distribution.
+	
 	static int findMinDiff(int arr[], int n,
 									int m)
 	{
-		// if there are no chocolates or
-		// number of students is 0
+		
 		if (m == 0 || n == 0)
 			return 0;
 	
 		// Sort the given packets
 		Arrays.sort(arr);
 	
-		// Number of students cannot be
-		// more than number of packets
+		
 		if (n < m)
 		return -1;
 	
 		// Largest number of chocolates
 		int min_diff = Integer.MAX_VALUE;
 	
-		// Find the subarray of size m
-		// such that difference between
-		// last (maximum in case of
-		// sorted) and first (minimum in
-		// case of sorted) elements of
-		// subarray is minimum.
+		
 		
 		for (int i = 0; i + m - 1 < n; i++)
 		{
@@ -44,7 +32,6 @@ class GFG {
 		return min_diff;
 	}
 	
-	/* Driver program to test above function */
 	public static void main(String[] args)
 	{
 		int arr[] = {12, 4, 7, 9, 2, 23,
@@ -60,4 +47,3 @@ class GFG {
 			
 	}
 }
-// This code is contributed by Arnav Kr. Mandal.
